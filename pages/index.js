7a08e324ba68bbadece5f4865bc5542a8afd25e8
crtroatan568-lab <crtroatan568@gmail.com>
2025-10-09 01:20:50 +0000

    Agrega carpeta pages y archivo index.js para Next.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Bienvenido a tu proyecto Next.js</h1>
+      <p>La carpeta <code>pages</code> ya existe y el build debería funcionar.</p>
+    </main>
+  );
+}
